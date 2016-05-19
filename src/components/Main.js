d46749876ab66bb007f800f490e5cac431fccacb
Luan James <capjam@gmail.com>
2016-05-19 10:52:35 -0300

    add materialize-css

--- src/components/Main.js
+++ src/components/Main.js
@@ -1,16 +1,22 @@
 require('normalize.css/normalize.css');
+require('materialize-css/dist/css/materialize.min.css');
+require('materialize-css/dist/js/materialize.min.js');
 require('styles/App.css');
 
 import React from 'react';
 
-let yeomanImage = require('../images/yeoman.png');
+import Navbar from './Navbar'
 
 class AppComponent extends React.Component {
   render() {
     return (
-      <div className="index">
-        <img src={yeomanImage} alt="Yeoman Generator" />
-        <div className="notice">Please edit <code>src/components/Main.js</code> to get started!</div>
+      <div>
+      	<Navbar />
+          <div className="fixed-action-btn" style={{bottom: '45px', right: '24px'}}>
+          	<a className="btn-floating btn-large red">
+          		<i className="material-icons">+</i>
+          	</a>
+          </div>
       </div>
     );
   }
@@ -20,3 +26,14 @@ AppComponent.defaultProps = {
 };
 
 export default AppComponent;
+<div class="fixed-action-btn" style="bottom: 45px; right: 24px;">
+          <a class="btn-floating btn-large red">
+            <i class="material-icons">mode_edit</i>
+          </a>
+          <ul>
+            <li><a style="transform: scaleY(0.4) scaleX(0.4) translateY(40px) translateX(0px); opacity: 0;" class="btn-floating red"><i class="material-icons">insert_chart</i></a></li>
+            <li><a style="transform: scaleY(0.4) scaleX(0.4) translateY(40px) translateX(0px); opacity: 0;" class="btn-floating yellow darken-1"><i class="material-icons">format_quote</i></a></li>
+            <li><a style="transform: scaleY(0.4) scaleX(0.4) translateY(40px) translateX(0px); opacity: 0;" class="btn-floating green"><i class="material-icons">publish</i></a></li>
+            <li><a style="transform: scaleY(0.4) scaleX(0.4) translateY(40px) translateX(0px); opacity: 0;" class="btn-floating blue"><i class="material-icons">attach_file</i></a></li>
+          </ul>
+        </div>
---
+++ src/components/Navbar.js
@@ -0,0 +1,21 @@
+import React from 'react';
+
+class NavbarComponent extends React.Component {
+  render() {
+    return (
+    	<nav>
+		    <div className="nav-wrapper">
+		      <a href="#" className="brand-logo"></a>
+		      <ul id="nav-mobile" className="right hide-on-med-and-down">
+		        <li><a href="collapsible.html">Test</a></li>
+		      </ul>
+		    </div>
+		  </nav>
+    );
+  }
+}
+
+NavbarComponent.defaultProps = {
+};
+
+export default NavbarComponent;
