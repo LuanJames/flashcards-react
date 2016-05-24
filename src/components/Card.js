9fc2a7efdb249843567e28f3fa7af1ffec8d80aa
Luan James <capjam@gmail.com>
2016-05-24 10:22:46 -0300

    changing card layout

--- src/components/Card.js
+++ src/components/Card.js
@@ -1,5 +1,31 @@
 import React from 'react';
 
+class InputComponent extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {text: ''};
+    this.onChange = this.onChange.bind(this);
+  }
+
+  onChange(e) {
+    this.setState({text: e.target.value});
+  }
+
+  getText() {
+    return this.state.text;
+  }
+
+  render() {
+    return (
+        <div className="input-field">
+          <textarea id="front" type="text" value={this.state.text} onChange={this.onChange} className="materialize-textarea" />
+          <label for="front">{this.props.title}</label>
+        </div>
+    );
+  }
+}
+
+
 class CardComponent extends React.Component {
   constructor(props) {
     super(props);
@@ -9,16 +35,22 @@ class CardComponent extends React.Component {
 
   render() {
     return (
-  <div className="card">
-    <div className="card-content">
-      <span className="card-title activator grey-text text-darken-4">Frente<i className="material-icons right">more_vert</i></span>
-      <p><a href="#">Click aki</a></p>
-    </div>
-    <div className="card-reveal">
-      <span className="card-title grey-text text-darken-4">Verso<i className="material-icons right">close</i></span>
-      <p>Nao sei o que eh</p>
-    </div>
-  </div>
+      <div className="card">
+        <div className="card-content">
+          <InputComponent ref="front" title="Frente" />
+          <span className="card-title activator grey-text text-darken-4">
+            <a className="waves-effect waves-light btn">
+              <i className="material-icons left">cloud</i>
+              Verso
+            </a>
+          </span>
+        </div>
+        <div className="card-reveal">
+          <InputComponent ref="back" title="Verso" />
+          <span className="card-title grey-text text-darken-4">Verso<i className="material-icons right">close</i></span>
+          <p>Nao sei o que eh</p>
+        </div>
+      </div>
     );
   }
 }
